965e2f70b80f56af9944d77d6d6faee762bc6e1c
Israel-Ferreira <israelfsouza@hotmail.com>
2023-08-16 14:40:58 -0300

    Exerc6

---
+++ Exerc6/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+string phrase = "O Rato Roeu a Roupa do Rei de Roma";
+
+string modifiedPhrase = SubstituirLetra(phrase, "A");
+
+Console.WriteLine(modifiedPhrase);
+
+
+string SubstituirLetra(string fraseAtual, string letra, string sep = " ")
+{
+    List<string> palavras = fraseAtual.Split(sep).ToList();
+    
+    List<String> variacoesLetra = new List<string>(){letra.ToUpper(), letra.ToLower()};
+
+    List<string> palavrasMapeadas = palavras.Select(wrd =>
+    {
+        string[] letters =  wrd.ToCharArray().Select(c => c.ToString()).ToArray();
+        var wrdModified = string.Join("", letters.Select(lt => variacoesLetra.Contains(lt) ? "&" : lt));
+        return wrdModified;
+    }).ToList();
+
+    return  string.Join(sep, palavrasMapeadas);
+}
