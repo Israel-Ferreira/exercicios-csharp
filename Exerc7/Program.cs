21ec9b35db33f6e14cd6714d2a220abf45a9d5c9
Israel-Ferreira <israelfsouza@hotmail.com>
2023-08-16 12:04:48 -0300

    Exercicios de dotnet

---
+++ Exerc7/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Informe o seu salário atual: ");
+double salarioAtual = double.Parse(Console.ReadLine()!);
+
+double salarioComReajuste = ReajustarSalario(salarioAtual);
+
+Console.WriteLine($"Salário Atual: {salarioAtual}");
+Console.WriteLine($"Salário com Reajuste: {salarioComReajuste}");
+
+
+
+double ReajustarSalario(double currentSalary)
+{
+    double incrementValue =  200.00;
+    if(currentSalary < 1700.00)
+    {
+        incrementValue = 300.00;
+    }
+    
+    return currentSalary + incrementValue;
+}
---
+++ Exerc8/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+namespace Exerc8;
+
+public class Program
+{
+    async static Task Main()
+    {
+        Console.WriteLine("Hello, World!");
+        
+        Console.Write("\nQual é o seu nome?: ");
+        string nome = Console.ReadLine()!;
+
+        Console.Write("\nQual é o seu email?: ");
+        string email = Console.ReadLine()!;
+
+        Console.Write("\nQual é o seu telefone?: ");
+        string telefone = Console.ReadLine()!;
+
+        Console.Write("\nDigite o seu RG: ");
+        string rg =  Console.ReadLine()!;
+        
+        string nomeArquivo =  "personal-info.txt";
+        
+        if(File.Exists(nomeArquivo))
+        {
+            File.Delete(nomeArquivo);
+        }
+        
+        using(FileStream fs = new FileStream(nomeArquivo, FileMode.CreateNew, FileAccess.ReadWrite))
+        {
+            string content =  $"Nome: {nome}\nEmail: {email}\nRG: {rg}\nTelefone: {telefone}";
+            byte[] contentEncoded  = EncodeValue(content);
+
+            await fs.WriteAsync(contentEncoded, 0, contentEncoded.Length);	
+        }
+        
+    }
+    
+    
+    static byte[] EncodeValue(string  strValue)
+    {
+        return new UTF8Encoding(true).GetBytes(strValue);
+    }
+}
